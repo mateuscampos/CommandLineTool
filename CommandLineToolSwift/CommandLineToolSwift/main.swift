d1f4903c13c095ceb9b9843dd7e2a73634dc60d4
Mateus Campos <mateuscampos@MacBook-Pro-de-Mateus.local>
2016-04-10 11:06:42 -0300

    creating project

---
+++ CommandLineToolSwift/CommandLineToolSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineToolSwift
+//
+//  Created by Mateus Campos on 10/04/16.
+//  Copyright © 2016 Mateus Campos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
