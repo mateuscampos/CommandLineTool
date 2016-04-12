08182cd481c7483953654324108a888b8ae2b920
Mateus Campos <mateus.campos@concrete.com.br>
2016-04-11 21:24:10 -0300

    refactor models

---
+++ CommandLineToolSwift/CommandLineToolSwift/Path.swift
@@ -0,0 +1,14 @@
+//
+//  Path.swift
+//  CommandLineToolSwift
+//
+//  Created by Mateus Campos on 11/04/16.
+//  Copyright © 2016 Mateus Campos. All rights reserved.
+//
+
+import Foundation
+
+
+class Path {
+    var filePath:String? = nil
+}
